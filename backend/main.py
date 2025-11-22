9189229e1b345341d276ac04ab53155ccf6a123f
liquoric <rezanovaleks3@gmail.com>
2025-11-22 15:22:57 +0500

    change struct

---
+++ backend/main.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from sqlalchemy import func
+
+from src.normalize_url import normalize_habr_url
+from src.parser import parse_article
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello from FastAPI backend!"}
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", os.getenv("FRONTEND_URL")],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
